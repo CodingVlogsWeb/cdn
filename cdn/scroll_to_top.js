b7bc280cb98d159678bf42dec6dabceb938073c5
CodingVlogsWeb <101495213+CodingVlogsWeb@users.noreply.github.com>
2022-11-11 18:30:45 +0530

    Update scroll_to_top.js

--- cdn/scroll_to_top.js
+++ cdn/scroll_to_top.js
@@ -5,6 +5,10 @@ const scrollContainer = () => {
   return document.documentElement || document.body;
 };
 
+backToTopButton.addEventListener("click", () => {
+    window.scrollTo({top: 0});
+})
+
 document.addEventListener("scroll", () => {
   if (scrollContainer().scrollTop > showOnPx) {
     backToTopButton.classList.remove("hidden")
