a9485937fd5c93b68f5b7005f13b3ad8cb2da023
CodingVlogsWeb <101495213+CodingVlogsWeb@users.noreply.github.com>
2022-11-11 18:03:48 +0530

    Create scroll_to_top.js

---
+++ cdn/scroll_to_top.js
@@ -0,0 +1,14 @@
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
